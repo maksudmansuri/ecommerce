901bde6acfb457501d21df81e10cb01afe279675
maksudmansuri <intellecttec@gmail.com>
2021-04-29 17:41:16 +0530

    errror 12

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'crispy_forms',
-    'ckeditor',
+    # 'ckeditor',
     'ckeditor_uploader',
     'accounts',
     'API',
