f69847439d01e5064d77e1bf139c97bf91cbc879
maksudmansuri <intellecttec@gmail.com>
2021-04-29 16:36:36 +0530

    error in heroku 9

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from API.import views
 # router.register(r'users', views.CustomUserViewSet)
 
 urlpatterns = [
-    path('dologin', views.dologin,name='dologin'),
+    path('', views.dologin,name='dologin'),
     # path('instructor_singup', views.instructor_singup,name='instructor_singup'),
     # path('student_singup', views.student_singup,name='student_singup'),
     # path('counsellor_singup', views.counsellor_singup,name='counsellor_singup'),
